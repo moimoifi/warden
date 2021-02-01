642138dc0acdb5bffb12b89dbed94132388b91e5
xQuicklyy <67281372+xQuicklyy@users.noreply.github.com>
2021-01-31 20:48:05 -0600

    Create MuteManager.java

---
+++ java/me/codex/warden/mutes/MuteManager.java
@@ -0,0 +1,38 @@
+package me.codex.warden.listeners;
+
+import me.codex.warden.Main;
+import me.codex.warden.infos.PlayerInfos;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
+
+public class PlayerJoin implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+        PlayerInfos playerInfos = Main.getInstance().playerInfos;
+
+        playerInfos.update(player);
+    }
+
+    @EventHandler
+    public void onLogin(PlayerLoginEvent e) {
+        Player player = e.getPlayer();
+
+        Main.getInstance().banManager.checkDuration(player.getUniqueId());
+
+        if(Main.getInstance().banManager.isBanned(player.getUniqueId())) {
+
+            e.setResult(PlayerLoginEvent.Result.KICK_BANNED);
+            e.setKickMessage("§cYou are banned from this server!\n " +
+                    "\n " +
+                    "§6Reason : §f" + Main.getInstance().banManager.getReason(player.getUniqueId()) + "\n " +
+                    "\n " +
+                    "§aTime left : §f" + Main.getInstance().banManager.getTimeLeft(player.getUniqueId()));
+        }
+    }
+
+}
