c43a9d4af51ba92cfb05706825035c2f3c5cdb09
xQuicklyy <67281372+xQuicklyy@users.noreply.github.com>
2021-01-31 20:46:54 -0600

    Create PlayerChat.java

---
+++ java/me/codex/warden/listeners/PlayerChat.java
@@ -0,0 +1,31 @@
+package me.codex.warden.listeners;
+
+import me.codex.warden.Main;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import java.util.UUID;
+
+public class PlayerChat implements Listener {
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onChat(AsyncPlayerChatEvent e) {
+        Player player = e.getPlayer();
+        UUID uuid = player.getUniqueId();
+
+        Main.getInstance().muteManager.checkDuration(uuid);
+
+        if(Main.getInstance().muteManager.isMuted(uuid)) {
+            e.setCancelled(true);
+            if(Main.getInstance().muteManager.getTimeLeft(uuid).equalsIgnoreCase("§cPermanent")) {
+                player.sendMessage("§cYou are permanently muted");
+            } else {
+                player.sendMessage("§cYou are muted for §e" + Main.getInstance().muteManager.getTimeLeft(uuid));
+            }
+        }
+    }
+
+}
